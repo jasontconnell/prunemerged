1019808e46aa7d89b6c60dec68591f0f72c0cd8e
Jason Connell <jconnell@delphicdigital.com>
2017-09-14 11:21:26 -0400

    Test commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "os/exec"
+)
+
+var branch string
+
+func init(){
+    flag.StringVar(&branch, "b", "", "Please provide the branch to compare (default develop)")
+}
+
+
+func main(){
+    flag.Parse()
+
+    if branch == "" {
+        fmt.Println("Usage ::  prunemerged -b [branch-name]")
+    }
+    cmd := exec.Command("git", "branch", "--merged", branch)
+
+    if b, err := cmd.Output(); err == nil {
+        fmt.Println("got read", string(b))
+    } else {
+        fmt.Println(err)
+    }
+}
