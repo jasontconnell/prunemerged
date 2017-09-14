f8edd7482a4daa2fb54169a5145dbf1d6366f334
Jason Connell <jconnell@delphicdigital.com>
2017-09-14 11:31:44 -0400

    Commit changes

--- main.go
+++ main.go
@@ -4,26 +4,34 @@ import (
     "fmt"
     "flag"
     "os/exec"
+    "strings"
 )
 
-var branch string
+var compareBranch string
 
 func init(){
-    flag.StringVar(&branch, "b", "", "Please provide the branch to compare (default develop)")
+    flag.StringVar(&compareBranch, "b", "", "Please provide the branch to compare (default develop)")
 }
 
 
 func main(){
     flag.Parse()
 
-    if branch == "" {
-        //fmt.Println("Usage ::  prunemerged -b [branch-name]")
-        branch = "develop"
+    if compareBranch == "" {
+        compareBranch = "develop"
     }
-    cmd := exec.Command("git", "branch", "--merged", branch)
+    cmd := exec.Command("git", "branch", "--merged", compareBranch)
 
     if b, err := cmd.Output(); err == nil {
-        fmt.Println("got read", string(b))
+        output := string(b)
+        branches := strings.Split(output, "\n")
+        for _, branch := range branches {
+            branch = strings.Trim(branch, " ")
+            if !strings.HasPrefix(branch, "* ") {
+                delLocal := exec.Command("git", "branch", "-d", branch)
+                delRemote := exec.Command("git", "push", "--delete", "origin", branch)
+            }
+        }
     } else {
         fmt.Println(err)
     }
