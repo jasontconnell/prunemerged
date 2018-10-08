1873e3283570bc19a5beb98295d493ed89b754dc
Jason Connell <jconnell@delphicdigital.com>
2018-10-08 12:57:16 -0400

    Updates, add dry run option, prune at the end.

--- main.go
+++ main.go
@@ -3,25 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"os/exec"
 	"regexp"
 	"strings"
 )
 
-var compareBranch string
-var ignorecsv string
 var ignore []string = []string{"master", "develop", "staging", "qa", "release", "uat", "production", "stage", "prod", "HEAD"}
 
-func init() {
-    ignoredDefault := ""
-    for _, i := range ignore {
-        ignoredDefault += i + " "
-    }
-    flag.StringVar(&compareBranch, "b", "", "Please provide the branch to compare (default develop)")
-    flag.StringVar(&ignorecsv, "i", "", "Branches to ignore, csv separated. Default:" + ignoredDefault)
-}
-
 func main() {
+	compareBranch := flag.String("b", "develop", "branch to compare (default develop)")
+	ignorecsv := flag.String("i", "", "branches to ignore, default: "+strings.Join(ignore, ", "))
+	dryrun := flag.Bool("dry", false, "only output commands to run")
+	//nopush := flag.Bool("nopush", false, "don't push changes to remote")
 	flag.Parse()
 
 	ignoreMap := make(map[string]string)
@@ -29,51 +23,73 @@ func main() {
 		ignoreMap[i] = i
 	}
 
-    for _, i := range strings.Split(ignorecsv, ","){
-        ignoreMap[i] = i
-    }
-
-	if compareBranch == "" {
-		compareBranch = "develop"
+	for _, i := range strings.Split(*ignorecsv, ",") {
+		ignoreMap[i] = i
 	}
 
-	cmd := exec.Command("git", "branch", "-a", "--merged", compareBranch)
+	cmd := exec.Command("git", "branch", "-a", "--merged", *compareBranch)
 	regex := regexp.MustCompile("(\\*)? +(remotes/)?(.*?)(\n| .*\n)")
 
-	if b, err := cmd.Output(); err == nil {
-		output := string(b)
-		matches := regex.FindAllStringSubmatch(output, -1)
-		for _, m := range matches {
-			isCurrent := m[1] == "*"
-			isRemote := m[2] == "remotes/"
+	b, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-			sp := strings.SplitN(m[3], "/", 2)
+	output := string(b)
+	matches := regex.FindAllStringSubmatch(output, -1)
+	for _, m := range matches {
+		isCurrent := m[1] == "*"
+		isRemote := m[2] == "remotes/"
 
-			remote := "origin"
-			branch := ""
+		sp := strings.SplitN(m[3], "/", 2)
 
-			if len(sp) == 2 {
-				remote, branch = sp[0], sp[1]
-			}
+		remote := "origin"
+		branch := ""
 
-			if _, ignoreBranch := ignoreMap[branch]; len(branch) == 0 || ignoreBranch || isCurrent || branch == compareBranch {
-				continue
-			}
+		if isRemote && len(sp) == 2 {
+			remote, branch = sp[0], sp[1]
+		} else if !isRemote {
+			remote, branch = "origin", m[3]
+		}
 
-			delLocal := exec.Command("git", "branch", "-d", branch)
-			delRemote := exec.Command("git", "push", "--delete", remote, branch)
+		if _, ignoreBranch := ignoreMap[branch]; len(branch) == 0 || ignoreBranch || isCurrent || branch == *compareBranch {
+			continue
+		}
 
+		delLocal := exec.Command("git", "branch", "-d", branch)
+		delRemote := exec.Command("git", "push", "--delete", remote, branch)
+		remotes := []string{}
+
+		shouldPrune := false
+
+		if *dryrun {
+			fmt.Println(remote, branch)
+		} else {
 			if isRemote {
-				if remoteOutput, remerr := delRemote.Output(); remerr != nil {
-					fmt.Println("command failed", delRemote, string(remoteOutput), remerr)
+				fmt.Println("git", "push", "--delete", remote, branch)
+				if remoteOutput, remerr := delRemote.CombinedOutput(); remerr != nil {
+					fmt.Println("command failed, removing remote", string(remoteOutput), remerr)
 				}
+
+				shouldPrune = true
+				remotes = append(remotes, remote)
 			} else {
-				if localOutput, locerr := delLocal.Output(); locerr != nil {
-					fmt.Println("command failed", delLocal, string(localOutput), locerr)
+				fmt.Println("git", "branch", "-d", branch)
+				if localOutput, locerr := delLocal.CombinedOutput(); locerr != nil {
+					fmt.Println("command failed, removing local", string(localOutput), locerr)
 				}
 			}
 		}
-	} else {
-		fmt.Println(err)
+
+		if shouldPrune {
+			for _, rem := range remotes {
+				prune := exec.Command("git", "remote", "prune", rem)
+				fmt.Println("git", "remote", "prune", rem)
+				if pruneOutput, pruerr := prune.CombinedOutput(); pruerr != nil {
+					fmt.Println("command failed, pruning remote", rem, string(pruneOutput), pruerr)
+				}
+			}
+		}
 	}
 }
